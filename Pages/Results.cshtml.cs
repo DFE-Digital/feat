2c5c8115328912a95f1a3bdafdd7e508cdd67d21
Stuart Foulds-Duncan <stuart.duncan@hippodigital.co.uk>
2025-07-07 12:11:27 +0100

    Updated to use service for search

--- Pages/Results.cshtml.cs
+++ Pages/Results.cshtml.cs
@@ -1,20 +1,19 @@
 using feat.web.Extensions;
 using feat.web.Models;
 using feat.web.Repositories;
+using feat.web.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SessionExtensions = Microsoft.AspNetCore.Http.SessionExtensions;
 
 namespace feat.web.Pages;
 
-public class ResultsModel(HttpClientRepository httpClientRepository) : PageModel
+public class ResultsModel(ISearchService searchService) : PageModel
 {
     public required Search Search { get; set; }
     
     public FindAResponse? FindAResponse { get; set; }
     
-    [BindProperty]
-    public FindARequest? FindARequest { get; set; }
     
     public async Task<IActionResult> OnGetAsync()
     {
@@ -24,14 +23,8 @@ public class ResultsModel(HttpClientRepository httpClientRepository) : PageModel
 
         Search.SetPage("Results");
         HttpContext.Session.Set("Search", Search);
-        
-        FindARequest = Search.ToFindARequest();
-        FindARequest.SessionId = HttpContext.Session.Id;
 
-        string url = "https://find-a-dev-def2ardzbwgvfzfm.westeurope-01.azurewebsites.net/api/Search";
-        // string url = "https://localhost:7121/api/Search";
-        
-        FindAResponse = await httpClientRepository.PostAsync<FindAResponse>(url, FindARequest);
+        FindAResponse = await searchService.Search(Search, HttpContext.Session.Id);
         
         return Page();
     }
