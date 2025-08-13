330ff1d125b3670ec4129d934c4ef131ce46e8da
Stuart Duncan <stuart.duncan@education.gov.uk>
2025-08-13 14:48:27 +0100

    Delete Services directory

--- Services/ISearchService.cs
+++
@@ -1,9 +0,0 @@
-using feat.web.Models;
-using Microsoft.AspNetCore.Mvc;
-
-namespace feat.web.Services;
-
-public interface ISearchService
-{
-    Task<FindAResponse> Search(Search search, string sessionId);
-}
