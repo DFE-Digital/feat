9a9882fded1abb63452e9931d91b55ff774a5a5f
AlCH7 <al.choudhury@hippodigital.co.uk>
2025-09-25 17:27:33 +0100

    FAB-410 - SQL and AI Search added in Terraform script
    * Added MS SQL database and Azure AI Search

---
+++ infrastructure/terraform/ms-az-search-service.tf
@@ -0,0 +1,8 @@
+resource "azurerm_search_service" "feat_search_service" {
+  name                = "${var.prefix}-search-svc"
+  resource_group_name = azurerm_resource_group.feat-rg.name
+  location            = azurerm_resource_group.feat-rg.location
+  sku                 = var.sku
+  replica_count       = var.replica_count
+  partition_count     = var.partition_count
+}
