bbc15c828d7fcd6796d94de5c0914475dfd22f7f
Chris Haslam <chris.haslam@hippodigital.co.uk>
2025-10-06 16:52:18 +0100

    FAB-308 - Inject SearchClient and EmbeddingClient into SearchService for easier testing

--- api/feat.api/Program.cs
+++ api/feat.api/Program.cs
@@ -1,7 +1,14 @@
+using System.Text.Json;
 using System.Text.Json.Serialization;
+using Azure;
+using Azure.AI.OpenAI;
+using Azure.Core.Serialization;
+using Azure.Search.Documents;
 using feat.api;
 using feat.api.Configuration;
 using feat.api.Services;
+using Microsoft.Extensions.Options;
+using OpenAI.Embeddings;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -24,6 +31,41 @@ builder.Services.AddControllers().AddJsonOptions(options =>
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+
+builder.Services.AddSingleton<SearchClient>(sp =>
+{
+    var options = sp.GetRequiredService<IOptions<AzureOptions>>().Value;
+    
+    var serializerOptions = new JsonSerializerOptions
+    {
+        Converters =
+        {
+            new JsonStringEnumConverter(),
+            new MicrosoftSpatialGeoJsonConverter()
+        },
+    };
+    
+    var clientOptions = new SearchClientOptions
+    {
+        Serializer = new JsonObjectSerializer(serializerOptions)
+    };
+    
+    return new SearchClient(
+        new Uri(options.AiSearchUrl),
+        options.AiSearchIndex,
+        new AzureKeyCredential(options.AiSearchKey),
+        clientOptions);
+});
+
+builder.Services.AddSingleton<EmbeddingClient>(sp =>
+{
+    var options = sp.GetRequiredService<IOptions<AzureOptions>>().Value;
+    
+    var openAiClient = new AzureOpenAIClient(
+        new Uri(options.OpenAiEndpoint), new AzureKeyCredential(options.OpenAiKey));
+    
+    return openAiClient.GetEmbeddingClient(options.OpenAiEmbeddingClientName);
+});
 
 builder.Services.AddSingleton<ISearchService, SearchService>();
 builder.Services.AddSingleton<ApiClient>();
