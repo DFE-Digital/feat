9aef909cd308462cecac2f8d4498a6851c5a91d8
alchoudhury <al.choudhury@hippodigital.co.uk>
2025-10-28 14:10:57 +0000

    Removed old screens

--- web/feat.web/Pages/Age.cshtml.cs
+++ web/feat.web/Pages/Age.cshtml.cs
@@ -7,33 +7,48 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace feat.web.Pages;
 
-public class AgeModel (ILogger<AgeModel> logger) : PageModel
+public class AgeModel(ILogger<AgeModel> logger) : PageModel
 {
     [BindProperty]
     [Required(ErrorMessage = "Please select your age group or select \"Skip this step\"")]
     public AgeGroup? AgeGroup { get; set; }
-    
+
     public required Search Search { get; set; }
-    
+
     public IActionResult OnGet()
     {
-        Search = HttpContext.Session.Get<Search>("Search") ?? new Search();
+        logger.LogInformation("OnGet called");
         
+        Search = HttpContext.Session.Get<Search>("Search") ?? new Search();
+
         if (Search.AgeGroup.HasValue)
             AgeGroup = Search.AgeGroup;
 
-        Search.SetPage(PageName.Age);//"Age");
+        Search.SetPage(PageName.Age);
         HttpContext.Session.Set("Search", Search);
-        
+
         return Page();
     }
 
     public IActionResult OnPost()
     {
-        logger.LogInformation("Age OnPost {AgeGroup}", AgeGroup);
+        logger.LogInformation("OnPost called");
+        
         try
         {
+            Search = HttpContext.Session.Get<Search>("Search") ?? new Search();
+            if (!ModelState.IsValid)
+                return Page();
 
+            if (AgeGroup.HasValue)
+                Search.AgeGroup = AgeGroup.Value;
+            
+            logger.LogInformation("Age OnPost {AgeGroup}", AgeGroup);
+            
+            Search.Updated = true;
+            HttpContext.Session.Set("Search", Search);
+
+            return RedirectToPage(PageName.CheckAnswers);
         }
         catch (Exception e)
         {
@@ -41,32 +56,5 @@ public class AgeModel (ILogger<AgeModel> logger) : PageModel
             Console.WriteLine(e);
             throw;
         }
-        Search = HttpContext.Session.Get<Search>("Search") ?? new Search();
-        if (!ModelState.IsValid)
-            return Page();
-        
-        //Search.SetPage("Age"); --?? 
-        Search.Updated = true;
-        
-        if (AgeGroup.HasValue) 
-            Search.AgeGroup = AgeGroup.Value;
-        
-        HttpContext.Session.Set("Search", Search);
-
-        /*
-        if (AgeGroup is Enums.AgeGroup.UnderEighteen or Enums.AgeGroup.EighteenToTwentyFour 
-            && SearchType == Enums.SearchType.HE)
-        {
-            return RedirectToPage("Interests");
-        }
-
-        if (AgeGroup == Enums.AgeGroup.UnderEighteen 
-            && SearchType is Enums.SearchType.FE or Enums.SearchType.Return)
-        {
-            return RedirectToPage("Location");
-        } check-answers
-        */
-
-        return RedirectToPage(PageName.CheckAnswers);// need to go to CheckAnswer => Summary.
     }
 }
