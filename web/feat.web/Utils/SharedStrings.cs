8ad0991c06d9a69b0125ca2bf118297b5effb049
alchoudhury <al.choudhury@hippodigital.co.uk>
2025-11-19 16:58:41 +0000

    FAB-517 Setting travel distance without location issue fixed

--- web/feat.web/Utils/SharedStrings.cs
+++ web/feat.web/Utils/SharedStrings.cs
@@ -7,7 +7,8 @@ public static class SharedStrings
     
     // Error Messages
     public const string LessThan100Char = "Please enter less than 100 characters";
-    public const string SelectHowFarTravel = "Please select how far you would be happy to travel.";
+    public const string SelectHowFarCanUTravel = "Please select how far you would be happy to travel.";
+    public const string EnterCityOrPostcode = "Enter a town, city or postcode to use the distance filter.";
     
     // Site Title
     public const string SiteTitle_ExploreYourOptions_GOVUK = "Explore your options after school or college - GOV.UK";
