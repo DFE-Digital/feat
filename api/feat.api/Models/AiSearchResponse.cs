6d52899f46619f7c4429f3a30307d394f109c350
Chris Haslam <chris.haslam@hippodigital.co.uk>
2025-12-02 11:49:51 +0000

    FAB-560 - Fix and refactoring of search result locations after AI Search change

--- api/feat.api/Models/AiSearchResponse.cs
+++ api/feat.api/Models/AiSearchResponse.cs
@@ -1,8 +1,12 @@
+using Microsoft.Spatial;
+
 namespace feat.api.Models;
 
 public class AiSearchResponse
 {
-    public required Guid Id { get; set; }
+    public required string Id { get; set; }
     
     public required string InstanceId { get; set; }
+    
+    public required GeographyPoint? Location { get; set; }
 }
