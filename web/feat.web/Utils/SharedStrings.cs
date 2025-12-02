24e995a9bf25023b24b4b81b3cb0c32e16f7205e
alchoudhury <al.choudhury@hippodigital.co.uk>
2025-12-02 09:28:16 +0000

    FAB-556 Qualification level screen is now set as mandatory

--- web/feat.web/Utils/SharedStrings.cs
+++ web/feat.web/Utils/SharedStrings.cs
@@ -6,6 +6,7 @@ public static class SharedStrings
     public const string LessThan100Char = "Please enter less than 100 characters";
     public const string SelectHowFarCanUTravel = "Please select how far you would be happy to travel.";
     public const string EnterCityOrPostcode = "Enter a town, city or postcode to use the distance filter.";
+    public const string SelectQualificationLevel = "Please select a qualification level";
     
     // Site Title
     public const string SiteTitle_ExploreYourOptions_GOVUK = "Explore your options after school or college - GOV.UK";
