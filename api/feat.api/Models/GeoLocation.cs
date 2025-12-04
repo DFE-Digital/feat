2c398ad814156b20f75354263106b105946271e6
KaraokeStu <stuart.duncan@education.gov.uk>
2025-12-04 14:17:09 +0000

    Added custom comparitors to enable distinct to work

--- api/feat.api/Models/GeoLocation.cs
+++ api/feat.api/Models/GeoLocation.cs
@@ -1,8 +1,27 @@
 namespace feat.api.Models;
 
-public class GeoLocation
+public class GeoLocation : IEquatable<GeoLocation>
 {
     public double Latitude { get; set; }
     
     public double Longitude { get; set; }
+
+    public bool Equals(GeoLocation? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Latitude.Equals(other.Latitude) && Longitude.Equals(other.Longitude);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((GeoLocation)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Latitude, Longitude);
+    }
 }
--- api/feat.api/Models/Location.cs
+++ api/feat.api/Models/Location.cs
@@ -1,6 +1,6 @@
 namespace feat.api.Models;
 
-public class Location
+public class Location : IEquatable<Location>
 {
     public string? Address1 { get; set; }
     
@@ -17,4 +17,23 @@ public class Location
     public string? Postcode { get; set; }
     
     public GeoLocation? GeoLocation { get; set; }
+
+    public bool Equals(Location? other)
+    {
+        if (other is null) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Address1 == other.Address1 && Address2 == other.Address2 && Address3 == other.Address3 && Address4 == other.Address4 && Town == other.Town && County == other.County && Postcode == other.Postcode && Equals(GeoLocation, other.GeoLocation);
+    }
+
+    public override bool Equals(object? obj)
+    {
+        if (obj is null) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        return obj.GetType() == GetType() && Equals((Location)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        return HashCode.Combine(Address1, Address2, Address3, Address4, Town, County, Postcode, GeoLocation);
+    }
 }
