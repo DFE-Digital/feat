ddb30caa8d262de3313dcbbc204b136f7f44a51e
varshakrishna-hippo <166136888+varshakrishna-hippo@users.noreply.github.com>
2026-02-24 13:57:56 +0000

    updated the Apprenticeship Course details page Test data

--- e2e/feat-playwright-e2e/src/ui/config/courseData.ts
+++ e2e/feat-playwright-e2e/src/ui/config/courseData.ts
@@ -8,7 +8,7 @@ export const courseData = {
     test: {
         ncs: 'bda91723-b65d-401b-ba0a-08de252620ac',
         degree: '64fd8867-90d0-42b3-3f71-08de31046717',
-        apprenticeship: '7e197c1a-3cc6-406d-a8f3-08de2796b116',
+        apprenticeship: '505b3c10-3ab9-49ff-6734-08de30ff5eb8',
     },
 
     beta: {
